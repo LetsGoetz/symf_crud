54474084543eefa1ffe9455c0d35f7f3d38ee243
LetsGoetz <61471416+LetsGoetz@users.noreply.github.com>
2020-04-20 18:25:29 +0200

    Create, Update, Delete

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -7,6 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ArticleController extends AbstractController
 {
@@ -25,6 +29,84 @@ class ArticleController extends AbstractController
     }
 
     /**
+     * @Route("/article/new", name="new_article")
+     * Method({"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $article = new Article();
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array(
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('body', TextAreaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Create',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $artticle = $form->getData();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('/articles/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/article/edit/{id}", name="edit_article")
+     * Method({"GET", "POST"})
+     */
+    public function edit(Request $request, $id)
+    {
+        $article = new Article();
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->find($id);
+
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class, array(
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('body', TextAreaType::class, array(
+                'required' => false,
+                'attr' => array('class' => 'form-control')
+            ))
+            ->add('save', SubmitType::class, array(
+                'label' => 'Update',
+                'attr' => array('class' => 'btn btn-primary mt-3')
+            ))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('/articles/edit.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+
+    /**
      * @Route("/article/{id}", name="article_show")
      */
     public function show($id)
@@ -36,6 +118,24 @@ class ArticleController extends AbstractController
         return $this->render('articles/show.html.twig', array(
             'article' => $article
         ));
+    }
+
+    /**
+     * @Route("/article/delete/{id}")
+     * @Method({"DELETE"})
+     */
+    public function delete(Request $request, $id)
+    {
+        $article = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->find($id);
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($article);
+        $entityManager->flush();
+
+        $response = new Response();
+        $response->send();
     }
 
     // # UNSAFE JUST FOR DISPLAY
